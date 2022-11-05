b5708fda54e9cd1982c7dff66011948507f6cb9f
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-05 18:58:31 +0100

    Site architecture

---
+++ propel-onscroll-border-radius/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-background-radius">
+        <div className="footer-content">
+          <h1>Sign up for our newsletter</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ propel-onscroll-border-radius/src/components/Main.tsx
@@ -0,0 +1,7 @@
+export const Main = () => {
+  return (
+    <div className="main-container">
+      <img src="https://images.unsplash.com/photo-1667513993054-9335374c6b94" />
+    </div>
+  );
+};
