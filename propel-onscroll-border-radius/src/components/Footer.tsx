dae073561414791ac59001a45e4c2cfeecb87028
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-05 21:07:01 +0100

    Add onscroll effect

--- propel-onscroll-border-radius/src/components/Footer.tsx
+++ propel-onscroll-border-radius/src/components/Footer.tsx
@@ -15,21 +15,25 @@ export const Footer = () => {
   const footerBackgroundRef = createRef<HTMLDivElement>();
 
   const handleScrollFooter = (event: Event) => {
-    const newRadius = scale(
-      footerRef?.current?.getBoundingClientRect().top || 0,
-      window.innerHeight * 0.1,
-      window.innerHeight * 0.9,
-      0,
-      50
-    );
-
-    // footerBackgroundRef.current?.style.backgrounRad;
+    if (footerRef.current && footerBackgroundRef.current) {
+      const newRadius = scale(
+        footerRef.current.getBoundingClientRect().top < window.innerHeight &&
+          footerRef.current.getBoundingClientRect().top >= 0
+          ? footerRef.current.getBoundingClientRect().top
+          : 0,
+        window.innerHeight * 0.1,
+        window.innerHeight * 0.9,
+        0,
+        50
+      );
+      footerBackgroundRef.current.style.borderRadius = `${newRadius}% ${newRadius}% 0 0`;
+    }
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScrollFooter);
 
-    () => {
+    return () => {
       window.removeEventListener("scroll", handleScrollFooter);
     };
   });
@@ -39,6 +43,38 @@ export const Footer = () => {
       <div className="footer-background-radius" ref={footerBackgroundRef}>
         <div className="footer-content">
           <h1>Sign up for our newsletter</h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input className="input-email" type="email" placeholder="Email" />
+            <input className="submit-btn" type="submit" value="Submit" />
+          </form>
+          <div className="footer-nav">
+            <div>
+              <h3>Discover</h3>
+              <a>Careers</a>
+              <a>News</a>
+              <a>Outboard motor</a>
+              <a>Inboard motor</a>
+            </div>
+            <div>
+              <h3>Propel</h3>
+              <a>About</a>
+              <a>Contact</a>
+              <a>Support</a>
+              <a>Media</a>
+            </div>
+            <div>
+              <h3>Social</h3>
+              <a>Instagram</a>
+              <a>Twitter</a>
+              <a>LinkedIn</a>
+            </div>
+            <div>
+              <h3>Legal</h3>
+              <a>Cookie notice</a>
+              <a>Privacy policy</a>
+              <a>Terms of service</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
