69ab53c16cb0f8c4990c592fac419c1d3308a235
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-11-05 19:08:21 +0100

    Footer

--- propel-onscroll-border-radius/src/components/Footer.tsx
+++ propel-onscroll-border-radius/src/components/Footer.tsx
@@ -1,7 +1,42 @@
+import { createRef, useEffect } from "react";
+
+function scale(
+  number: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number
+) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
 export const Footer = () => {
+  const footerRef = createRef<HTMLDivElement>();
+  const footerBackgroundRef = createRef<HTMLDivElement>();
+
+  const handleScrollFooter = (event: Event) => {
+    const newRadius = scale(
+      footerRef?.current?.getBoundingClientRect().top || 0,
+      window.innerHeight * 0.1,
+      window.innerHeight * 0.9,
+      0,
+      50
+    );
+
+    // footerBackgroundRef.current?.style.backgrounRad;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollFooter);
+
+    () => {
+      window.removeEventListener("scroll", handleScrollFooter);
+    };
+  });
+
   return (
-    <div className="footer-container">
-      <div className="footer-background-radius">
+    <div className="footer-container" ref={footerRef}>
+      <div className="footer-background-radius" ref={footerBackgroundRef}>
         <div className="footer-content">
           <h1>Sign up for our newsletter</h1>
         </div>
